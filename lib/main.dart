9bc8b96704de2df6ee7d106239a7bba178640ae9
yug soni <yug.spider@gmail.com>
2022-05-30 21:49:28 +0530

    Day2 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcome to 30 days of flutter"),
-          ),
-        ),
-      ),
-    );
+    return MaterialApp(home: HomePage(),);
   }
 }
